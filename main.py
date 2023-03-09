99c4c657794e717e36b48eaae2f6df4c3bd2feb7
Shalini Nannapaneni <nshalini262@gmail.com>
2023-03-09 11:27:26 -0500

    first commit

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 def main():
-
+    # This function takes each value in the password and converts it to an integer and then adds 3 to it.
+    # Then all the integer values are added to a string. The string is then returned.
     def encode(password):
         string = ""
         for i in password:
             i = 3 + int(i)
             string = string + str(i)
-        print(string)
+        return string
 
 
 if __name__ == '__main__':
